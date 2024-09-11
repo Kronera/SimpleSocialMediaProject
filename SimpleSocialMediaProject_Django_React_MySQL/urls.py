5874ef841c7fdf7aab156fe97d6b1d1bb96a5d39
Natan Najera <125765564+Kronera@users.noreply.github.com>
2024-09-11 01:14:58 -0700

    Added React frontend to the project
    Set up the React frontend with create-react-app.
    Added .gitignore to exclude node_modules and environment files.
    Generated initial package.json and related files for managing dependencies.
    Prepared project for further frontend development.

--- SimpleSocialMediaProject_Django_React_MySQL/urls.py
+++ SimpleSocialMediaProject_Django_React_MySQL/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin panel URL
     path('', include('myapp.urls')),  # Include your app's URLs (replace 'myapp' with the name of your app)
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
